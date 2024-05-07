a1668ac7b18f49857f6293741e2c166fa4c03fc0
lozzo <wuyouget@gmail.com>
2024-05-07 22:02:17 +0800

    feat: deploy dist only

--- README.md
+++
@@ -1,6 +0,0 @@
-# build
-```shell
-npn run build
-```
-`dist` 目录下的文件就是构建后的文件，直接用这个文件去部署githb pages即可
-
---
+++ assets/404-CprHYXhp.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-D8N42jml.js";function r(){return t.jsx("div",{children:"404"})}export{r as default};
